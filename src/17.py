bb89ec7608cb9c0133971000f0c524cf35760dd2
elmoghazyshaden <elmoghazyshaden@gmail.com>
2025-03-24 07:04:02 +0000

    Add 17.py

---
+++ src/17.py
@@ -0,0 +1,12 @@
+def calculate_area(radius):
+    """
+    Calculate the area of a circle given its radius.
+    
+    Parameters:
+        radius (float): The radius of the circle.
+        
+    Returns:
+        float: The area of the circle.
+    """
+    return 3.14159 * radius ** 2
+
